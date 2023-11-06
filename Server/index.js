44e23fd146e66259178ff05f869f43ed1ea0da28
GoldenSnakeWebDev <goldensnake.web.dev@gmail.com>
2023-11-07 04:41:24 +0900

    using socket.io

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,9 @@
 const express = require('express');
-// const axios = require('axios');
-// const fetch = require('node-fetch');
-const https = require('https');
+const http = require('http');
 const mysql = require('mysql');
 const cron = require('node-cron');
 const crypto = require('crypto');
-const TempMail = require("node-temp-mail");
-const TM = require('temp-mail-api');
-const { generateEmail, getInbox, deleteMail } = require('./temp_email');
+const cheerio = require('cheerio');
 
 const cors = require('cors');
 const { default: axios } = require('axios');
@@ -15,15 +11,111 @@ const { default: axios } = require('axios');
 const app = express();
 const port = 5000;
 
-app.use(cors());
-
 app.use(express.json());
+
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
+
+const server = http.createServer(app);
+const { Server, Socket } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+
+  console.log('New client connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+  socket.on('message', (data) => {
+    console.log('Received message:', data);
+    // Process received message or emit it to other clients
+  });
+
+  socket.on('refresh', () => {
+
+    console.log('refresh!');
+    get_messages();
+  })
+});
 
 app.get('/', function (req, res) {
     console.log('test okay');
 })
 
+cron.schedule('* * * * *', () => {
+  get_messages();
+  // getMessageList();
+});
 
+server.listen(port, async () => {
+  
+  console.log(`Proxy server listening at http://localhost:${port}`);
+}); 
+
+app.post('/register_email', async (req, res) => {
+
+    console.log('back end test okay!!!');
+    
+    let email_address = req.body.email_addresss;
+
+    try {
+      const response = await axios.get('https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=1');
+      const [emailData] = response.data;
+
+      email_address = response.data[0];
+
+      const { login, domain } = emailData;
+      const tempEmail = `${login}@${domain}`;
+      console.log(`Temporary Email: ${tempEmail}`);
+    } catch (error) {
+      console.error(error);
+    }
+
+
+    
+    const login = email_address.split("@")[0];
+    const temp_email = login + "@vjuum.com";
+    
+    console.log("address>>>>", temp_email);
+
+    const connection = mysql.createConnection({
+      host: 'localhost', // hostname
+      user: 'root',      // username
+      password: '',  // password
+      database: 'temp_email_managment' // database name
+    });
+
+    connection.connect((error) => {
+      if (error) {
+        console.log('Error connecting to the database: ', error);
+      } else {
+        console.log('Connected to the database');
+      }
+    });
+
+    const query = 'INSERT INTO emails (email_address) VALUES (?)';
+    connection.query(query, temp_email, (error) => {
+      if (error) {
+        console.log('Error inserting data: ', error);
+      } else {
+        console.log('Data inserted successfully');
+      }
+    });
+
+    res.status(200).send({"state":"okay", "address":temp_email});
+
+});
 function get_messages () {
 
   console.log("running task every min");
@@ -46,91 +138,71 @@ function get_messages () {
     if (error) {
       console.error('Error executing query: ', error);
     } else {
-      console.log('Query results: ', results);
 
+      console.log("length of results>>>", results.length);
       if (results.length > 0) {
-        results.map(async email => {
+        results.map(async (email) => {
+          // io.emit('message', {email});
+          try {
 
-          const email1 = new TM('temp-email');
+            const login = email.email_address.split("@")[0];
+            const domain = email.email_address.split("@")[1];
 
-          email1.ready((email, error) => {
-            if (!error) {
-              console.log(`Email address is ${email}`);
-          
-              // Getting email list
-              email1.getEmails((emails, error) => {
-                if (!error) {
-                  console.log('Email list :', emails);
-                } else console.error(error);
-              });
-                    
-            } else console.error(error);
-          });
+            const response = await axios.get(`https://www.1secmail.com/api/v1/?action=getMessages&login=${login}&domain=${domain}`);
+            const emailData = response.data;
 
-          // console.log("hash>>>>", getMD5Hash(email.email_address));
-          // const account = new TempMail(`${email.email_address}`);
+            
+            if (emailData.length > 0) {
+              emailData.map(async message => {
+                const messaged_time = new Date(message.date);
+                const curTime = new Date();
+                const message_id = message.id;
+                // const response1 = await axios.get(`https://www.1secmail.com/api/v1/?action=readMessage&login=${login}&domain=${domain}&id=${message_id}`);
+                // const body = response1.data.body;
 
-          // // console.log("address", account.getAddress());
-          // account.getMail().then((messages) => {
-          //   console.log(messages);
-          // });
-          const inbox = await getInbox(email.email_address);
+                // console.log("body>>>", body);
+                if (curTime - messaged_time < 28860000) {
 
-          console.log("inbox>>>>", inbox);
+                  const subject=  message.subject;
+
+                  console.log("subject>>>", subject);
+
+                  if (subject === 'Verify your email address') {
+                    const response = await axios.get(`https://www.1secmail.com/api/v1/?action=readMessage&login=${login}&domain=${domain}&id=${message_id}`);
+                    const body = response.data.body;
+
+                    const $ = cheerio.load(body);
+                    const aTags = $('a');
+
+                    aTags.each((index, element) => {
+                      const textContent = $(element).text();
+
+                      console.log("content>>>", textContent);
+
+                      if (textContent === "Verify Email") {
+                        const verify_address = $(element).attr('href');
+                        console.log("verify address>>>", verify_address);
+                        io.emit('verify_email', {verify_address, "email_address":email.email_address});
+                        return false;
+                      }
+                    })
+
+                  } else {
+                    io.emit("message", {subject});
+                  }
+
+                }
+                
+              })
+            }
+            // console.log("response>>>>>", response.data);
+            
+          } catch (error) {
+            console.error(error);
+          }
 
         })
       }
     }
   });
 }
-
-function getMD5Hash(email) {
-  const md5Hash = crypto.createHash('md5');
-  md5Hash.update(email);
-
-  return md5Hash.digest('hex');
-}
-
-cron.schedule('* * * * *', () => {
-  get_messages();
-});
-app.post('/register_email', async (req, res) => {
-
-    console.log('back end test okay!!!');
-
-    const email_address = req.body.email_addresss;
-
-    console.log("address>>>>", email_address);
-
-    const connection = mysql.createConnection({
-      host: 'localhost', // hostname
-      user: 'root',      // username
-      password: '',  // password
-      database: 'temp_email_managment' // database name
-    });
-
-    connection.connect((error) => {
-      if (error) {
-        console.log('Error connecting to the database: ', error);
-      } else {
-        console.log('Connected to the database');
-      }
-    });
-
-    const query = 'INSERT INTO emails (email_address) VALUES (?)';
-    connection.query(query, email_address, (error) => {
-      if (error) {
-        console.log('Error inserting data: ', error);
-      } else {
-        console.log('Data inserted successfully');
-      }
-    });
-
-});
-
-app.listen(port, async () => {
-
-  console.log(await generateEmail(10, "asd"));
-
-  console.log(`Proxy server listening at http://localhost:${port}`);
-}); 
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,16 @@
 import './LandingPage.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import socketIOClient from 'socket.io-client';
+import io from 'socket.io-client';
+import { toast } from 'react-toastify';
 
 function LandingPage () {
 
   const [text_userInput, setText_userInput] = useState('')
+  const [socket, setSocket] = useState(new io());
+  const [verify_address, setVerifyAddress] = useState('');
+
   const handle_textarea = (event) => {
     setText_userInput(event.target.value);
   }
@@ -23,6 +29,8 @@ function LandingPage () {
               .then((response) => {
                 
                 console.log('response>>>>>', response.data);
+
+                setText_userInput(response.data.address);
                 
               }).catch((error) => {
                 if (error.response) {
@@ -38,12 +46,75 @@ function LandingPage () {
           }
     }
 
+    const refresh = () => {
+
+      console.log("refresh");
+      socket.emit('refresh');
+    }
+
+    socket.on('message', (data) => {
+      console.log("received message:", data);
+      // alert(data.subject);
+      toast(data.subject);
+      if (Notification.permission === "granted") {
+        new Notification("you received a new message", {body: `${data.subject}`});
+      }
+    })
+
+    socket.on('verify_email', (data) => {
+      setVerifyAddress(data.verify_address);
+      setText_userInput(data.email_address);
+      // alert("verify address!");
+      toast("verify address");
+      if (Notification.permission === "granted") {
+        new Notification("you received a new message", {body: `${data.subject}`});
+      }
+
+    })
+
+    useEffect(() => {
+
+      if (Notification.permission !== "granted") {
+        Notification.requestPermission().then((permission) => {
+          if (permission !== "granted") {
+            console.log('notification is not allowed');
+          }
+        })
+      }
+
+      const newsocket = io.connect('http://localhost:5000');
+      newsocket.on('connect', () => {
+        console.log('Connected to server');
+      });
+      
+      newsocket.on('disconnect', () => {
+        console.log('Disconnected from server');
+      });
+      
+      // socket.on('message', (data) => {
+      //   console.log('Received message:', data);
+      //   // Process received message or update component state
+      // });
+
+      setSocket(newsocket);
+      
+    }, [])
 
     return (
-        <div id='main-board'>
-            <textarea id="user-input-text" value={text_userInput} onChange={handle_textarea}></textarea>
-            <button onClick={handle_test}>register</button>
-        </div>
+      <>
+          <div id='main-board'>
+              <textarea id="user-input-text" value={text_userInput} onChange={handle_textarea}></textarea>
+              <button onClick={handle_test}>register</button>
+          </div>
+          <div id='verify_address'>
+            <label>verify email address</label>
+              <input type='text' value={verify_address} style={{width: '50%'}}></input>
+          </div>
+
+          <div id='refresh'>
+            <button onClick={refresh}>refresh</button>
+          </div>
+        </>
     )
 }
 
